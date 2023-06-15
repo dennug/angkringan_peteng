88184ff2dac11044ca9dc860511423a6f598f7e3
Muhamad-naufal <muhamadnaufal594@gmail.com>
2023-06-15 17:42:39 +0700

    perbaikan 1

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -10,7 +10,7 @@
             <form action="?" method="get" class="form-inline">
                 <div class="input-group ml-auto">
                     <input type="text" name="search" class="form-control" placeholder="Search"
-                        values="<?php= request()->search ?> ?> ?> ?> ?>">
+                        values="<?php= request()->search ?> ?> ?>">
                     <div class="input-group-append">
                         <button class="btn btn-outline-secondary" type="submit">Cari..</button>
                     </div>
@@ -37,7 +37,6 @@
                             <td>{{ $row->role }}</td>
                         </tr>
                     @endforeach
-
                 </tbody>
             </table>
         </div>
